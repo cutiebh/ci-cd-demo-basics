73cd248c7a9f174bea457bed5951311c7c5ea17d
cutiebh <cutiebh@gmail.com>
2025-11-04 16:55:45 +0000

    1st change request
    Added multiply and divide functions

--- src/main.py
+++ src/main.py
@@ -3,3 +3,11 @@ def add(a, b):
 
 def subtract(a, b):
     return a-b
+
+#Added by Cutie
+def multiply(a, b):
+    return a*b
+
+#Added by Cutie
+def divide(a, b):
+    return a/b
--- tests/test_main.py
+++ tests/test_main.py
@@ -10,3 +10,16 @@ def test_subtract_function():
     assert subtract(6, 3) == 3
     assert subtract(0, 0) == 0
     assert subtract(5, 5) == 0
+
+#Added by Cutie
+def test_multiply_function():
+    assert multiply(2, 3) == 6
+    assert multiply(0, 0) == 0
+    assert multiply(5, 5) == 1
+    assert multiply(9, 1) == 9
+
+def test_divide_function():
+    assert divide(10, 2) == 5
+    assert divide(0, 0) == 0
+    assert divide(5, 5) == 1
+    assert divide(9, 1) == 9
